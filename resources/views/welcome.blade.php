c9158395ecad2c2577824358a561dae3953ab7ba
mikaeltreppo <mikaeltreppo@outlook.com>
2023-05-11 17:38:39 +0200

    card messe

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,12 +19,11 @@
 @foreach ($movies as $movie)
     
 
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+    <div class="card d-inline-flex p-2 m-3" style="width: 18rem;">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <h5 class="card-title">{{$movie->title}}</h5>
+          <p class="card-text">Nazionality: {{$movie->nationality}}</p>
+          <a href="#" class="btn btn-primary">Link al film</a>
         </div>
       </div>
       @endforeach
